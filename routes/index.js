8ae235f3289b6ddca44ca3cac7438ab892daafb9
Alfarisg 34 <alfarisg34@gmail.com>
2021-05-29 13:09:52 +0700

    bug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const filmsController = require('../controllers/FilmController');
+const { getFilms, getGenreFilm, getFilmByGenre } = require('../controllers/FilmController');
 
-router.get('/filmku',filmsController.getFilms);
-router.get('/filmku/:id', filmsController.getFilms);
+router.get('/filmku',getFilms);
+router.get('/filmku/:id', getFilms);
 router.get('/genres', getGenreFilm)
 router.get('/filmsByGenre/:genre', getFilmByGenre)
 
